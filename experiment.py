42db45104432df275dd9867a0fdd32bcfac86c8e
Anthony James Dempsey <anthonyjdempsey@gmail.com>
2016-12-01 22:22:20 +0000

    Use wrapper function
    Use the wrapper function provided by curses to make the code a bit
    safer.

--- experiment.py
+++ experiment.py
@@ -1,12 +1,20 @@
 #! /usr/bin/python3
 
 import curses
+from curses import wrapper
 
-myscreen = curses.initscr()
+TAB_WIDTH = 8
 
-myscreen.border(0)
-myscreen.addstr(12, 25, "Python curses in action!")
-myscreen.refresh()
-myscreen.getch()
+def main(myscreen):
+    myscreen.clear()
+    
+    #myscreen.border(0)
+    myscreen.addstr(12, 0, "Python curses in action!")
+    myscreen.addstr(13, TAB_WIDTH, "A new line of text?!")
+    myscreen.refresh()
+    myscreen.getch()
 
-curses.endwin()
+    curses.endwin()
+
+if __name__ == '__main__':
+    wrapper(main)

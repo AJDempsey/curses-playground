681fbba848506c895938ea177affa0008928c9e8
Anthony Dempsey <adempsey@brocade.com>
2016-12-09 13:35:51 +0000

    Remove old code we're no longer using
    Tidied up experiment.py a little removing code we don't use any more.
    Some of the code might be useful for other sections but isn't needed
    here.

--- experiment.py
+++ experiment.py
@@ -17,64 +17,18 @@ def read_snippet_from_file(file_name):
         file_string = "{0}".format(err)
     return (status, file_string)
 
-def find_next_bound(line_list, bounding_char):
-    y, x = curses.getsyx()
-    cursor_x = x
-    if y > len(line_list):
-        return y, x
-    for line in line_list[y:]:
-        print("y: "+str(y)+" x: "+str(x)+" bounding char is "+bounding_char)
-        if x > len(line):
-            x = 0
-        x = line.find(bounding_char, x)
-        if x == cursor_x:
-            x = line.find(bounding_char, x+1)
-        print("Found "+bounding_char+" at y "+str(y)+" x "+str(x))
-        if x < 0:
-            x = 0
+def user_loop(screen):
+    test_snippet = Snippet(myscreen)
+    test_snippet.update_screen()
+    user_input = myscreen.getkey()
+    while user_input != "\n":
+        if user_input == "\t":
+            test_snippet.move_to_next_edit_token()
+        elif user_input == "KEY_BTAB":
+            test_snippet.move_to_previous_edit_token()
         else:
-            break
-        y += 1
-    return (y, x)
-
-def highlight_next_template(screen, options, working_list):
-    y, x = find_next_bound(working_list, "<")
-    screen.move(y, x)
-    screen.refresh()
-    end_y, end_x = find_next_bound(working_list, ">")
-
-    if y >= len(working_list) or end_x + 1 > len(working_list[y]):
-        return y, x
-    overwrite_string = working_list[y][x:end_x+1]
-    # Highlight the current template we're editing.
-    screen.addnstr(y, x, overwrite_string, (end_x - x+1), curses.color_pair(curses.COLOR_YELLOW))
-    screen.move(y, x)
-    win = options['win']
-    win.mvwin(y, x)
-    screen.refresh()
-    win.refresh()
-
-def user_loop(screen, options, working_list):
-    next_char = screen.getkey()
-
-    while next_char != "\n":
-        # Build the text string using new characters
-        string = options['txt']
-        string = string + next_char
-        print("Next char: "+next_char)
-
-        # A <tab> is used to move to the next template
-        if next_char == "\t":
-            highlight_next_template(screen, options, working_list)
-
-        # Otherwise we refresh the window with the text
-        else:
-            win = options['win']
-            win.addnstr(0, 0, string, len(string))
-            win.refresh()
-            screen.refresh()
-            options['txt'] = string
-            next_char = screen.getkey()
+            test_snippet.update_token_string(user_input)
+        user_input = myscreen.getkey()
 
 def main(myscreen):
 
@@ -88,42 +42,15 @@ def main(myscreen):
         print("Problem reading the snippet")
         print(file_string)
         return
-    #myscreen.border(0)
     working_list = file_string.split( "\n")
     line_num = 0
     if curses.has_colors():
         curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_GREEN) # Line is ok, passes validation?
         curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED) # Line is not ok, doesn't pass validation?
 
-    #for line in working_list:
-    #    myscreen.addstr(line_num, 0, line)
-#        line_num += 1
-
-    # Create the options window#
-#    win = curses.newwin(6, 6, 1, 10)
-#    options = {}
-#    options['win'] = win
-#    options['txt'] = ''
-
-#    highlight_next_template(myscreen, options, working_list)
-#    user_loop(myscreen, options, working_list)
-#    curses.endwin()
-    test_snippet = Snippet(myscreen)
-    test_snippet.update_screen()
-    user_input = myscreen.getkey()
-    while user_input != "\n":
-        if user_input == "\t":
-            test_snippet.move_to_next_edit_token()
-        elif user_input == "KEY_BTAB":
-            test_snippet.move_to_previous_edit_token()
-        else:
-            test_snippet.update_token_string(user_input)
-        user_input = myscreen.getkey()
+    user_loop(myscreen, options, working_list)
 
     print(test_snippet)
-    #print( str(y)+" "+str(x))
-    #print( str(end_y)+" "+str(end_x))
-    #print(overwrite_string)
 
 if __name__ == '__main__':
     wrapper(main)

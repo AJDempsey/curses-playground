a724e4ece8c3f7373918eee9ff01d00c6c238d16
Anthony James Dempsey <anthonyjdempsey@gmail.com>
2017-02-28 21:30:04 +0000

    Add argparser code and update variables to constants
    Started the code to make the tool more dynamic. It now requires a file
    containing a snippet to be passed to it. Right now the snippet is
    ignored but this will be updated in a later commit.
    Also updated variables in the global scope to be constants to stop
    pylint complaining.

--- example.py
+++ example.py
@@ -11,6 +11,7 @@ import curses
 from curses import wrapper
 import time
 import threading
+import argparse
 
 import tool_tip
 from snippet import Snippet
@@ -55,7 +56,8 @@ def user_loop(screen, tool_tip_thread):
             test_snippet.insert_new_edit_token()
         elif user_input[:3] == "KEY" and user_input != "KEY_BACKSPACE":
             tool_tip_string = "Key stroke {} not recognized".format(user_input)
-            y_coord, _ = screen.getmaxyx()  # We don't need the x variable of the screen size, so ignore it.
+            # We don't need the x variable of the screen size, so ignore it.
+            y_coord, _ = screen.getmaxyx()
             tt_instance = tool_tip.Tool_tip(y_coord -1, 0, 1, len(tool_tip_string)+1)
             tt_instance.add_to_window(tool_tip_string)
             tt_instance.activate()
@@ -69,28 +71,46 @@ def user_loop(screen, tool_tip_thread):
         user_input = screen.getkey()
     print(test_snippet)
 
-def main(myscreen, tool_tip_thread):
+def main(myscreen, tool_tip_thread, snippet_file):
     """
     Main function to initialise curses and call the user function.
     """
+
     stdout_filename = open('debug.log', 'w')
     sys.stdout = stdout_filename
 
     curses.start_color()
     myscreen.clear()
-    status, file_string = read_snippet_from_file("for-loop.snip")
+    status = 0
+    if snippet_file is not None:
+        status, file_string = read_snippet_from_file(snippet_file)
     if status < 0:
         print("Problem reading the snippet")
         print(file_string)
         return
     if curses.has_colors():
-        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_GREEN) # Line is ok, passes validation?
-        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED) # Line is not ok, doesn't pass validation?
+        # Line is ok, passes validation?
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_GREEN)
+        # Line is not ok, doesn't pass validation?
+        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED)
 
     user_loop(myscreen, tool_tip_thread)
 
 if __name__ == '__main__':
-    tool_tip_thread = None
-    wrapper(main, tool_tip_thread)
-    if tool_tip_thread is not None:
-        tool_tip_thread.join()
+    PARSER = argparse.ArgumentParser(description=\
+        "Example program showing how to use a snippet.\n"+\
+        "Navigating a snippet is easy:\n"+\
+        "Tab - Move to next editable template.\n"+\
+        "Shift + Tab - Move to previous editable template.\n"+\
+        "Insert - Add new editable token under the current line.\n"+\
+        "Enter - Execute the snippet as it currently exists and exit.",\
+        formatter_class=argparse.RawTextHelpFormatter)
+    PARSER.add_argument("--snippet-file")
+    ARGS = PARSER.parse_args()
+    TOOL_TIP_THREAD = None
+    if ARGS.snippet_file is None:
+        print("No snippet file given, exiting.")
+        exit()
+    wrapper(main, TOOL_TIP_THREAD, ARGS.snippet_file)
+    if TOOL_TIP_THREAD is not None:
+        TOOL_TIP_THREAD.join()
